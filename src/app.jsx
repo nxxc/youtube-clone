e49b74db0ce743a4c6844ff5e340d46d7a4106e2
nxxc <nomad.nxxc@gmail.com>
2021-01-05 17:01:16 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import './app.css';
-import Main from './pages/main.jsx';
+import VideoList from './components/video_list/video_list.jsx';
 
 function App() {
-  return <Main />;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDDazUfGdBl_DEUKV94I9-lOe989LN_6cw',
+      requestOptions,
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const VideoItem = (props) => {
+  return <h1>{props.video.snippet.title}</h1>;
+};
