0f347e854f981a58b40dfb7be9959f75b45afdc2
nxxc <nomad.nxxc@gmail.com>
2021-01-06 12:50:54 +0900

    [style] Add CSS

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './video_detail.module.css';
 
 const VideoDetail = ({ video }) => {
+  console.log(video);
   return (
     <section className={styles.detail}>
       <iframe
